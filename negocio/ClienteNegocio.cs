84a8f53b2fba5a6d8d90a1022aad17e52fc9f534
Rafael <rafael.castro@mi.unc.edu.ar>
2025-01-04 02:43:39 -0300

    Seteo del proyecto

---
+++ negocio/ClienteNegocio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class ClienteNegocio
+    {
+        private static ClienteNegocio instancia = null;
+        public ClienteNegocio()
+        {
+
+        }
+
+        public static ClienteNegocio Instancia
+        {
+            get
+            {
+                if (instancia == null) 
+                {
+                    instancia = new ClienteNegocio();
+                }
+                return instancia;
+            }
+        }
+
+        public List<Cliente> Listar(out string mensaje)
+        {
+            mensaje = "";
+            List<Cliente> clientes = new List<Cliente>();
+            try
+            {
+                clientes = Cliente.Listar();
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+            }
+            return clientes;
+        }
+    }
+}
